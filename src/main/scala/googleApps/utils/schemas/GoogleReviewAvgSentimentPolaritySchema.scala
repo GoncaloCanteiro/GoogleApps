f53665af24a5cb6af093d86f5b2931ecdc34f852
GoncaloCanteiro <gcanteiro15@gmail.com>
2023-11-28 11:57:04 +0000

    chore: remove unnecessary schema files and test samples

--- src/main/scala/googleApps/utils/schemas/GoogleReviewAvgSentimentPolaritySchema.scala
+++
@@ -1,9 +0,0 @@
-package googleApps.utils.schemas
-
-import org.apache.spark.sql.types.{DoubleType, StructField, StructType}
-
-class GoogleReviewAvgSentimentPolaritySchema {
-  val schema = StructType(Array(
-    StructField("Average_Sentiment_Polarity", DoubleType),
-  ))
-}
